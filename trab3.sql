e8388e9a41b4159c2c0a1f99678500b1a0b0141c
Rllucena <129807139+Rllucena@users.noreply.github.com>
2023-11-24 21:11:11 -0300

    Update trab3.sql

--- trab3.sql
+++ trab3.sql
@@ -4,37 +4,41 @@ DROP DATABASE IF EXISTS Garagem;
 
 CREATE DATABASE Garagem;
 
-\c Garagem;
+--\c Garagem;
 
 CREATE TABLE Cliente (
     id serial primary key,
     cpf character(11) unique,
     nome character varying(100) not null,
     email character varying(100) unique,
-    senha character varying(100) not null    
-    data_nascimento date 
-    endereco varchar(150) not null
+    senha character varying(100) not null,    
+    data_nascimento date NOT NULL, 
+    rua VARCHAR(100),
+    bairro VARCHAR(100),
+    complemento VARCHAR(100),
+    numero INT
 );
 
 CREATE TABLE Modelo (
     id serial primary key,
     --titulo character varying (150) not null,
     descricao text not null,
-    ano_lancamento integer
+    ano_lancamento integer NOT NULL
 );
 
 CREATE TABLE Veiculo (
     id serial primary key,
     chassi character varying (100) not null,
-    placa character varying (7) not null,
+    placa character varying (10) not null,
     cor character varying (30) not null,
     ano integer not null,
     Cliente_id integer references Cliente (id),
-    Modelo_id integer references Modelo (id),
+    Modelo_id integer references Modelo (id)
 );
 
 CREATE TABLE andar (
     id serial primary key,
+    numero_vagas INT NOT NULL
   
 );
 
@@ -46,7 +50,7 @@ CREATE TABLE vaga (
 CREATE TABLE vaga_veiculo (
     vaga_id integer references vaga (id),
     veiculo_id integer references Veiculo (id),
-    data_hora_entrada timestamp default current_timestamp,
+    data_hora_entrada timestamp default current_timestamp NOT NULL,
     data_hora_saida timestamp default current_timestamp,
     valor_pago money default 0,
     primary key (vaga_id, Veiculo_id)
@@ -55,10 +59,58 @@ CREATE TABLE vaga_veiculo (
 
 
 
+INSERT INTO Cliente (id,cpf, nome, email, senha, data_nascimento, rua, bairro, complemento, numero)
+VALUES
+  (1,'22345678901', 'João a Silva', 'joaoo@email.com', 'senha1231', '1981-05-15', 'Rua A', 'Centro', 'Apto 10', 1),
+  (2,'18765432101', 'Maria b Oliveira', 'mariao@email.com', 'senha4561', '1993-08-20', 'Rua B', 'Bairro X', 'Casa 2', 4),
+  (3,'15555555555', 'Pedro s Santos', 'pedroo@email.com', 'senha7891', '1984-02-10', 'Rua C', 'Bairro Y', 'Sala 3', 7);
+
+-- Inserindo dados na tabela Modelo
+INSERT INTO Modelo (id,descricao, ano_lancamento)
+VALUES
+  (1,'Sedan', 2020),
+  (2,'SUV', 2019),
+  (3,'Hatchback', 2022);
+
+-- Inserindo dados na tabela Veiculo
+INSERT INTO Veiculo (id,chassi, placa, cor, ano, Cliente_id, Modelo_id)
+VALUES
+  (1,'ABC19345678901234', 'XYZ-1234', 'Azul', 2021, 1, 1),
+  (2,'DEFg6789012345678', 'ABC-5678', 'Vermelho', 2019, 2, 2),
+  (3,'HId90123456789012', 'JKL-9012', 'Verde', 2022, 3, 3);
+  
+INSERT INTO andar (id,numero_vagas)
+VALUES
+  (1,10),
+  (2,15),
+  (3,20);
+
+-- Inserindo dados na tabela Vaga
+INSERT INTO vaga (id,andar_id)
+VALUES
+  (1,1),
+  (2,1),
+  (3,2),
+  (4,2),
+  (5,2),
+  (6,3),
+  (7,3),
+  (8,3),
+  (9,3);
+
+-- Inserindo dados na tabela vaga_veiculo
+INSERT INTO vaga_veiculo (vaga_id, veiculo_id, data_hora_entrada, data_hora_saida, valor_pago)
+VALUES
+  (1, 1, '2023-11-24 08:00:00', '2023-11-24 18:00:00', 15.00),
+  (3, 2, '2023-11-25 10:30:00', NULL, NULL),
+  (7, 3, '2023-11-26 14:45:00', '2023-11-26 17:30:00', 10.50);  
+
+
+
 
 
 --1)
-Select * from Veiculo id =1
+SELECT placa, ano FROM Veiculo WHERE id = 1;
 
 --2)
 SELECT * FROM Veiculo where ano => 2000;
